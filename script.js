05907458868696acf20010b406d898d1e496f8d5
Nicole <snmcgovern17@gmail.com>
2021-03-29 10:19:40 -0400

    added js for required feilds in form with for loop and changed responsive menu to right

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ function myFunction() {
   }
 }
 
+
+
+function checkForm(form) {
+  var inputs = form.getElementsByTagName('input');
+  for (var i = 0; i < inputs.length; i++) {
+    if(inputs[i].hasAttribut("required")){
+      if(inputs[i].value == ""){
+        alert("Please fill out all required fields.")
+        return false;
+      }
+    }
+  }
+  alert("Information has been received.")
+  return true;
+}
+
 function validEmail() {
   var x=document.getElementById("email").value;
   if (x == "" ) {
-    alert("Not valid email");
+    alert("Not a valid email");
   } else {
-    alert("Information has been received");
+    alert("Information has been received.");
   }
 }
 
